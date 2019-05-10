5d1b21ab5795c75bcfb355ff69f125923ca3197d
liuke123 <1443174787@qq.com>
2019-05-10 17:38:29 +0800

    驾车路径规划

--- js/total.js
+++ js/total.js
@@ -1,13 +1,49 @@
 var map = new AMap.Map('container',{
-    center:[116.39, 39.9]
+    center:[116.39, 39.9],
+    zoom:14
 })
+
 /*添加/去除缩放组件*/
 var toolbar;
-AMap.plugin(['AMap.ToolBar'],function(){//异步加载插件
+/*测距插件*/
+var rule;
+/*驾车规划插件*/
+var driving;
+/*自动补全插件*/
+var completestart;
+var completeEnd;
+/*起点坐标*/
+var POIstrat = [];
+var POIenf = [];
+AMap.plugin(['AMap.ToolBar','AMap.MouseTool','AMap.Driving','AMap.Autocomplete'],function(){//异步加载插件
     toolbar = new AMap.ToolBar({
         offset:new AMap.Pixel(10,30)
     });
+
+    rule = new AMap.MouseTool(map);
+
+    completestart= new AMap.Autocomplete({
+        city: '全国',
+        input: 'tipinputstart'
+    });
+    completestart.on('select',function(AutocompleteResult){
+        POIstrat[0] = AutocompleteResult.poi.location.lng;
+        POIstrat[1] = AutocompleteResult.poi.location.lat;
+    })
+    completeEnd= new AMap.Autocomplete({
+        city: '全国',
+        input: 'tipinputEnd'
+    });
+    completeEnd.on('select',function(AutocompleteResult){
+        POIenf[0] = AutocompleteResult.poi.location.lng;
+        POIenf[1] = AutocompleteResult.poi.location.lat;
+    })
+
+
+
 })
+
+
 function bindingToolBar(val){
     if(val.checked){/*添加*/
         map.addControl(toolbar);
@@ -28,7 +64,6 @@ function showMenu(){
 }
 window.onload = function(){
     document.getElementsByClassName('table')[0].addEventListener('webkitTransitionEnd',function () {
-        console.log('过渡结束');
         if(show == true){
             document.getElementsByClassName('btn')[0].innerHTML = '收起'
         }else{
@@ -85,4 +120,115 @@ function bindingMarker(val){
 /*清除所有marker*/
 function clearAllMarker(){
     map.remove(markerArr)
+}
+
+/*操作圆形*/
+var polylineEditor;
+var circleArr = [];
+var contextMenu = new AMap.ContextMenu();
+var _this;
+contextMenu.addItem("删除", function () {
+    map.remove(_this)
+    if(polylineEditor){
+        polylineEditor.close()
+    }
+}, 0);
+contextMenu.addItem("编辑", function () {
+    if(polylineEditor){
+        polylineEditor.close()
+    }
+    addplugin(_this)
+}, 1);
+/*添加编辑插件*/
+function addplugin(_this){
+    map.plugin('AMap.CircleEditor',function(){
+        polylineEditor = new AMap.CircleEditor(map, _this);
+        polylineEditor.open();
+    })
+}
+var drawCircle = function(e){
+    var circle = new AMap.Circle({
+        center: e.lnglat,  // 圆心位置
+        radius: 1000, //半径
+        borderWeight: 3,
+        strokeColor: "#FF33FF",
+        strokeOpacity: 1,
+        strokeWeight: 6,
+        strokeOpacity: 0.2,
+        fillOpacity: 0.4,
+        strokeStyle: 'dashed',
+        strokeDasharray: [10, 10],
+        // 线样式还支持 'dashed'
+        fillColor: '#1791fc',
+        zIndex: 50,
+        map:map
+    });
+    /*点击矢量图，可以删除和编辑矢量图*/
+    circle.on('click',function(e){
+        _this = this;
+        contextMenu.open(map, e.lnglat);
+    })
+    circleArr.push(circle);
+}
+
+/*操作线段*/
+function changeVector(val){
+    if(val.value==1){
+        map.on('click',drawCircle)
+        rule.close(false)
+    }else{
+        map.off('click',drawCircle)
+        rule.rule()
+    }
+}
+
+/*驾车路线规划*/
+function getRoad(){
+    console.log('POIstrat',POIstrat);
+    console.log('POIenf',POIenf);
+    driving = new AMap.Driving({
+        map:map,
+        // 驾车路线规划策略，AMap.DrivingPolicy.LEAST_TIME是最快捷模式
+        policy: AMap.DrivingPolicy.LEAST_TIME
+    })
+    var startLngLat = POIstrat
+    var endLngLat = POIenf
+    driving.search(startLngLat, endLngLat, function (status, result) {
+        // 未出错时，result即是对应的路线规划方案
+
+        /*console.log('status',status);
+        console.log('result',result);*/
+    })
+}
+
+/*left中tab切换*/
+function changeTab(val) {
+    var child = document.getElementsByClassName('right')[0].childNodes;
+    var b = child;
+    for (var i = 0; i < b.length; i++) {
+        if (b[i].nodeName == "#text" && !/\s/.test(b.nodeValue)) {
+            document.getElementsByClassName('right')[0].removeChild(b[i]);
+        }
+    }
+    for(let i = 0;i<b.length;i++){
+        if(i==val){
+            b[i].style.display = 'block'
+        }else{
+            b[i].style.display = 'none'
+        }
+    }
+    /*管理对map点击事件的监听*/
+    if(val==0){
+        map.off('click',drawCircle)
+        rule.close(false)
+    }
+    if(val==1){
+        map.off('click',markerClick)
+        map.on('click',drawCircle)
+    }
+    if(val==2){/*取消监听，并取消所有覆盖层*/
+        map.off('click',markerClick)
+        map.off('click',drawCircle)
+        rule.close(false)
+    }
 }
